12540a119f7132d3c25dca575de59a5d13921098
hnil8 <2311061193@hunre.edu.vn>
2026-04-22 21:27:12 +0700

    Delete CinemaManagement.BLL/Services directory

--- CinemaManagement.BLL/Services/IProductService.cs
+++
@@ -1,9 +0,0 @@
-using CinemaManagement.BLL.DTOs;
-
-namespace CinemaManagement.BLL.Services;
-
-public interface IProductService
-{
-    Task<List<NhomFnBDto>> GetAllNhomWithSanPhamAsync();
-    Task<List<ComboDto>> GetAllCombosAsync();
-}
