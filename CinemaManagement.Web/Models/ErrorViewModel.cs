b57e101aebb6a7edd5ae435615d7ab83a2cb4c73
hnil8 <2311061193@hunre.edu.vn>
2026-05-04 21:58:46 +0700

    Delete CinemaManagement.Web directory

--- CinemaManagement.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CinemaManagement.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- CinemaManagement.Web/Models/TicketViewModels/BookingConfirmationViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace CinemaManagement.Web.Models.TicketViewModels;
-
-public class BookingConfirmationViewModel
-{
-    public int HoaDonId { get; set; }
-    public bool IsAuthenticated { get; set; }
-}
